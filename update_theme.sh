4037764ff7e518ed74bc90ea3c6d4aa7f236c5d2
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-02-22 12:45:52 -0600

    Update assets while both showing and saving logs

--- update_theme.sh
+++ update_theme.sh
@@ -3,4 +3,8 @@ source /edx/app/edxapp/edxapp_env
 rm -rf /edx/app/edxapp/edx-platform/themes/cmeonline-theme
 mkdir /edx/app/edxapp/edx-platform/themes/cmeonline-theme
 cp -r /home/ubuntu/edx-theme.cmeonline.com/cmeonline-theme/* /edx/app/edxapp/edx-platform/themes/cmeonline-theme
+
+cd /edx/app/edxapp/edx-platform
+paver update_assets lms --settings=aws | tee ~/custom-theme-lms.log
+paver update_assets cms --settings=aws | tee ~/custom-theme-cms.log
 EOF
