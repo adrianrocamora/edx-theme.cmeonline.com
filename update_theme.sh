c5dd0460e5bfc97676fb4c53f0be037a9cfd6698
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-02-22 14:34:44 -0600

    Restarting services for full theme update

--- update_theme.sh
+++ update_theme.sh
@@ -8,3 +8,9 @@ cd /edx/app/edxapp/edx-platform
 paver update_assets lms --settings=aws | tee ~/custom-theme-lms.log
 paver update_assets cms --settings=aws | tee ~/custom-theme-cms.log
 EOF
+
+/edx/bin/supervisorctl status
+/edx/bin/supervisorctl restart lms
+/edx/bin/supervisorctl restart cms
+/edx/bin/supervisorctl restart edxapp_worker:
+/edx/bin/supervisorctl status
